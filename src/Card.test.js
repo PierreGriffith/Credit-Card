4c377a288ba2f1125495317261babafdc5c616a1
PierreGriffith <parisianpierre@yahoo.com>
2019-01-14 10:38:32 +0100

    unit testing is working and the interfaces is done.

--- src/Card.test.js
+++ src/Card.test.js
@@ -1,4 +1,36 @@
-import fs from 'fs';
 import  Card from './Card'
+import { mount } from 'enzyme';
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
 
-jest.mock('fs');
+
+configure({ adapter: new Adapter() });
+
+
+test('testing visa card', () => {
+
+    const wrapper = mount(
+        <Card />
+    );
+
+    wrapper.find('.card-number').simulate('change', {target: {value: '4'}});
+    const result = wrapper.find('.input-card').text();
+   expect(result).toBe('visa');
+
+
+});
+
+
+test('testing a wrong card', () => {
+
+    const wrapper = mount(
+        <Card />
+    );
+
+    wrapper.find('.card-number').simulate('change', {target: {value: 'a'}});
+    const result = wrapper.find('.input-card').text();
+    expect(result).toBe('unregognizable card');
+
+
+});
